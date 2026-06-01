8034956b09bb2d2142b22cdbf0c8b563488ddecd
Bernardusz <b.christian.nugroho.study@gmail.com>
2026-06-01 19:35:17 +0700

    Initial commit: character-stat - Spring Boot Week 18

---
+++ backend/src/main/java/io/github/bernardusz/character_stat/CharacterStatApplication.java
@@ -0,0 +1,13 @@
+package io.github.bernardusz.character_stat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterStatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterStatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/bernardusz/character_stat/CharacterStatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bernardusz.character_stat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterStatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
